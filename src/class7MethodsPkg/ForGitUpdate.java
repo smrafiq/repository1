83a25615bf6532b5fec95be2db844061ebcf4d2f
micr0tect@NA <joejesper22@gmail.com>
2023-10-01 23:09:02 -0400

    added a new class in the project

---
+++ src/class7MethodsPkg/ForGitUpdate.java
@@ -0,0 +1,10 @@
+package class7MethodsPkg;
+
+public class ForGitUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("update code for git repository");
+	}
+
+}
